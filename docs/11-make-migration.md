e67f365964f00769d7fec92e475122e6bf80e6e3
Joel Lau <joel.lau@protonmail.com>
2021-03-18 21:47:06 +0800

    added notes on performing migration

--- docs/11-make-migration.md
+++ docs/11-make-migration.md
@@ -2,6 +2,5 @@
 
 > Database migrations are a way to safely update your database schema both locally and on production. Instead of running the doctrine:schema:update command or applying the database changes manually with SQL statements, migrations allow to replicate the changes in your database schema in a safe manner. - [Source](https://symfony.com/doc/current/bundles/DoctrineMigrationsBundle/index.html)
 
-```bash
-symfony console make:migration
-```
+1. Create migration file: `symfony console make:migration`
+2. Perform migration : `symfony console doctrine:migrations:migrate`
--- migrations/Version20210318132605.php
+++ migrations/Version20210318132605.php
@@ -12,19 +12,19 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210318132605 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE stock_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE stock (id INT NOT NULL, symbol VARCHAR(4) NOT NULL, currency VARCHAR(3) NOT NULL, exchange_name VARCHAR(30) NOT NULL, price DOUBLE PRECISION NOT NULL, price_change DOUBLE PRECISION NOT NULL, previous_close DOUBLE PRECISION DEFAULT NULL, region VARCHAR(3) NOT NULL, short_name VARCHAR(30) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
